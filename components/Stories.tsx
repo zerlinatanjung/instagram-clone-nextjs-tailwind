8e63576c8a29156ba838b59d6819b644526df8d9
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-11 20:21:47 +0700

    refactor: add type Suggestion and implement Story component

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
 import { ContextualCard } from '@faker-js/faker/helpers'
+import Story from './Story'
+
+type Suggestion = {
+  id: number
+} & ContextualCard
 
 function Stories() {
-  const [suggestions, setSuggestions] = useState<ContextualCard[]>([])
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([])
   useEffect(() => {
     const suggestions = [...Array(10)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
@@ -14,7 +19,14 @@ function Stories() {
   }, [])
 
   return (
-    <div>
+    <div className="mt-8 flex space-x-2 overflow-x-scroll rounded-sm border border-gray-200 bg-white p-6">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
       {/* Stories */}
       {/* Stories */}
       {/* Stories */}
