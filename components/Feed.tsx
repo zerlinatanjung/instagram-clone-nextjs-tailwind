1bb6969634bdd03ea8979f20f81ed09f43f8926b
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-13 22:52:52 +0700

    refactor: change style for Feed component and register Posts component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,12 @@
+import Posts from './Posts'
 import Stories from './Stories'
 
 function Feed() {
   return (
-    <main className="max-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
+    <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
       <section className="cal-span-2">
-        {/* Stories */}
         <Stories />
-        {/* Posts */}
+        <Posts />
       </section>
 
       {/* Section*/}
