947236ade2a99b249290f23ced864b373b892046
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-13 22:50:41 +0700

    refactor: add style for Story component

--- components/Story.tsx
+++ components/Story.tsx
@@ -8,8 +8,12 @@ type StoryProps = {
 function Story({ img, username }: StoryProps) {
   return (
     <div>
-      <img src={img} alt="" />
-      <p>{username}</p>
+      <img
+        className=" h-14 w-14 transform cursor-pointer rounded-full border-2 border-red-500 object-contain p-[1.5px] transition duration-200 ease-out hover:scale-110"
+        src={img}
+        alt=""
+      />
+      <p className="w-14 truncate text-center text-xs">{username}</p>
     </div>
   )
 }
