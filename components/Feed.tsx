708aa982c132c7636476c09639ab47e9bf3bcde7
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-18 17:17:11 +0700

    Refactor: implementation MiniProfile and Suggestions on Feed component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,18 +1,21 @@
 import Posts from './Posts'
 import Stories from './Stories'
+import MiniProfile from './MiniProfile'
+import Suggestions from './Suggestions'
 
 function Feed() {
   return (
     <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
-      <section className="cal-span-2">
+      <section className="col-span-2">
         <Stories />
         <Posts />
       </section>
 
-      {/* Section*/}
-      <section>
-        {/* Mini profile */}
-        {/* Suggestions */}
+      <section className="hidden md:col-span-1 xl:inline-grid">
+        <div className="fixed top-20 ">
+          <MiniProfile />
+          <Suggestions />
+        </div>
       </section>
     </main>
   )
