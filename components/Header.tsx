0712c049bbefae761fa036f8ebee72943f7a309c
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-18 17:16:05 +0700

    refactor: change src url img on Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -59,7 +59,7 @@ function Header() {
           <HeartIcon className="navBtn" />
 
           <img
-            src="https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_W13BgAJ8IYAX_cAhzt&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT-SdWCIM-9_33CfNjp_km41KdgX-8n3DZOCauqjyFzl_g&oe=625749A6"
+            src="https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Bau7iezywHgAX-2WxiN&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT8PG6b21ogkMcFsvTGDy2u2FjtD9rrA-3caxTl6bWjKCw&oe=62612CE6"
             alt="profile picture"
             className="h-10 cursor-pointer rounded-full"
           />
