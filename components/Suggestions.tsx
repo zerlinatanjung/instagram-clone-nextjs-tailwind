3bcb2fabeb50f68f9f78fc2c0467b3a02428f55c
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-18 17:15:25 +0700

    feat: add Suggestions components

---
+++ components/Suggestions.tsx
@@ -0,0 +1,46 @@
+import faker from '@faker-js/faker'
+import { ContextualCard } from '@faker-js/faker/helpers'
+import { profile } from 'console'
+import React, { useEffect, useState } from 'react'
+type Suggestion = {
+  id: number
+} & ContextualCard
+
+function Suggestions() {
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([])
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }))
+    setSuggestions(suggestions)
+  }, [])
+  return (
+    <div className="mt-4 ml-10">
+      <div className="mb-5 flex justify-between text-sm">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="font-semibold text-gray-600">See All</button>
+      </div>
+      {suggestions.map((profile) => (
+        <div
+          key={profile.id}
+          className="mt-3 flex items-center justify-between "
+        >
+          {' '}
+          <img
+            className="h-10 w-10 rounded-full border p-[2px] "
+            src={profile.avatar}
+            alt=""
+          />
+          <div className="ml-4 flex-1">
+            <h2 className="text-sm font-semibold ">{profile.username}</h2>
+            <h3>Works at {profile.company.name}</h3>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Suggestions
