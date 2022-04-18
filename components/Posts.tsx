d6cdc34d5b81d940ef3175e0d87ebc6df37d5503
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-18 17:19:25 +0700

    refactor: register Post into Posts component and change posts data

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import Post from './Post'
 
-const DUMMY_DATA = [
+const posts = [
   {
     id: '123',
     username: 'zerlinatanjung',
     userImg:
-      'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_W13BgAJ8IYAX_cAhzt&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT-SdWCIM-9_33CfNjp_km41KdgX-8n3DZOCauqjyFzl_g&oe=625749A6',
+      'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Bau7iezywHgAX-2WxiN&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT8PG6b21ogkMcFsvTGDy2u2FjtD9rrA-3caxTl6bWjKCw&oe=62612CE6',
+    img: 'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Bau7iezywHgAX-2WxiN&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT8PG6b21ogkMcFsvTGDy2u2FjtD9rrA-3caxTl6bWjKCw&oe=62612CE6',
+    caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm!',
+  },
+  {
+    id: '123',
+    username: 'zerlinatanjung',
+    userImg:
+      'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Bau7iezywHgAX-2WxiN&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT8PG6b21ogkMcFsvTGDy2u2FjtD9rrA-3caxTl6bWjKCw&oe=62612CE6',
+    img: 'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/272868296_2553996868073503_7533612638628071323_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Bau7iezywHgAX-2WxiN&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT8PG6b21ogkMcFsvTGDy2u2FjtD9rrA-3caxTl6bWjKCw&oe=62612CE6',
+    caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm!',
   },
 ]
 
 function Posts() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
     </div>
   )
 }
