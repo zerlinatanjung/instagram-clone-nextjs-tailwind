172da3d55b0573d0203c6ba3a8f5e05ad558a057
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-21 13:29:55 +0700

    feat: add Modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+
+function Modal() {
+  const [open, setOpen] = useRecoilState(modalState)
+
+  return <div>{open && <p>THE MODAL IS OPEN</p>}</div>
+}
+
+export default Modal
