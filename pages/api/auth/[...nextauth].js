fa23e08992500302c80b8613c2169aaed33ce502
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-21 13:26:00 +0700

    refactor: change next_public env and add callback for session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,12 +5,23 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
   pages: {
     signIn: '/auth/signin',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split(' ')
+        .join('')
+        .toLocaleLowerCase()
+
+      session.user.uid = token.sub
+      return session
+    },
   },
 })
