0102c802e995a3e981a39b6884c5991fa9fc4000
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-21 13:28:16 +0700

    refactor: implementation Modal into index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 
 const Home: NextPage = () => {
   return (
-    <div className="scrollbar-hide h-screen overflow-y-scroll bg-gray-50">
+    <div className="h-screen overflow-y-scroll bg-gray-50 scrollbar-hide">
       <Head>
         <title>Instagram 2.0 Youtube</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Modal />
       <Header />
       <Feed />
-      {/* Modal */}
     </div>
   )
 }
