64e512d03f44e73c022b65134cc11af1a4a391ce
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-04-23 14:39:34 +0700

    refactor: fix typescript error on Posts component

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,19 +1,35 @@
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { Snapshot } from 'recoil'
 import { db } from '../firebase'
 import Post from './Post'
 
+interface Post {
+  id: string
+  image: string
+  caption: string
+  username: string
+  profileImg: string
+}
+
 function Posts() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState<Post[]>([])
 
   useEffect(
     () =>
       onSnapshot(
         query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
         (snapshot) => {
-          console.log('Snapshot', snapshot)
-          setPosts(snapshot.docs)
+          setPosts(
+            snapshot.docs
+              .filter((x) => (x.data() as Post).image !== undefined)
+              .map(
+                (x) =>
+                  ({
+                    ...x.data(),
+                    id: x.id,
+                  } as Post)
+              )
+          )
         }
       ),
     [db]
@@ -21,16 +37,18 @@ function Posts() {
 
   return (
     <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          username={post.data().username}
-          userImg={post.data().profileImg}
-          img={post.data().image}
-          caption={post.data().caption}
-        />
-      ))}
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            id={post.id}
+            username={post.username}
+            userImg={post.profileImg}
+            img={post.image}
+            caption={post.caption}
+          />
+        )
+      })}
     </div>
   )
 }
